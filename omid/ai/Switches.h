b1afc81832ab33321ca78ae5887df39bb0e186dd
fdmxfarhan <fdmxfarhan@yahoo.com>
2021-10-13 17:53:20 -0400

    10

--- omid/ai/Switches.h
+++ omid/ai/Switches.h
@@ -67,6 +67,6 @@
 #define PORT_NUM_SEND_ERforce_COMMAND_BLUE		10301
 //#define GROUP_ADDR_SEND_ERforce_COMMAND	"172.25.0.12"//div a
 //#define GROUP_ADDR_SEND_ERforce_COMMAND	"172.20.0.6"//div c
-#define GROUP_ADDR_SEND_ERforce_COMMAND	"172.20.0.16"
+#define GROUP_ADDR_SEND_ERforce_COMMAND	"172.19.0.11"
 
 #endif
